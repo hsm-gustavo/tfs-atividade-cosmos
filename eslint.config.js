191efd424a2412a762514f549cc431b135ef25c6
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2026-05-22 12:58:03 -0300

    chore: add repo config to ensure different environments will maintain the same pattern

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
+
+module.exports = [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  prettier,
+
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+];
